783583c9e05f3fff30b858b788afb91d9414355f
ToltalTouch <micaeldoamaral@gmail.com>
2025-07-03 22:26:43 -0300

    atualizacao e novos arquivos

--- disc_bot/disc_bot.py
+++ disc_bot/disc_bot.py
@@ -1,9 +1,13 @@
 from replit_run import keep_alive
+import subprocess
 import logging
 import os
 import discord
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s = %(levelname)s: %(message)s')
+
+username = os.getlogin()
+logging.info(f"{username}")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -24,6 +28,14 @@ async def on_message(message):
         aviso = 'O servidor do Minebobos está online, abrindo o minecraft de geral...'
         await message.channel.send(aviso)
         logging.info(aviso)
+        try:
+            subprocess.run([f'C:\\Users\\{username}\\AppData\\Roaming\\.minecraft\\TLauncher.exe'])
+            logging.info("Minecraft aberto com sucesso.")
+            await message.channel.send("Minecraft aberto com sucesso!")
+        except Exception as e:
+            logging.error(f"Erro ao abrir o Minecraft: {e}")
+            await message.channel.send("Houve um erro ao abrir o Minecraft.")
+
 
 keep_alive()
-client.run(os.getenv('DISCORD_TOKEN'))
+client.run(os.getenv('DISCORD_KEY'))
